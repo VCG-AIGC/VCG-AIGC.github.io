5e1c8e294322d32ee2a55d4b3f7530d54c620894
Zhihao Hu <30406852+ZhihaoHu@users.noreply.github.com>
2023-06-07 20:43:38 +0800

    V2.0

--- static/js/index.js
+++ static/js/index.js
@@ -31,11 +31,11 @@ $(document).ready(function() {
 
     var options = {
 			slidesToScroll: 1,
-			slidesToShow: 2,
+			slidesToShow: 3,
 			loop: true,
 			infinite: true,
 			autoplay: true,
-			autoplaySpeed: 5000,
+			autoplaySpeed: 8000,
     }
 
 		// Initialize all div with carousel class
